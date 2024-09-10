73c9abdf3af49316d62446cfcaf2fef79aa2907f
marixl <marianavalenca72@gmail.com>
2024-09-10 10:26:51 -0300

    Add files via upload

---
+++ 04-nome-idade.py
@@ -0,0 +1,15 @@
+# atribuição de variáveis
+nome = "Mariana"
+idade = 16
+altura = 1.56
+
+# exibir mensagem na tela
+print("nome:", nome, "idade:", idade, "altura:", altura)
+# formatação com f-string
+print(f"none: {nome} idade: {idade} altura: {altura}")
+
+nome = input("Digite o seu nome:")
+idade = int(input("Digite a sua idade:"))
+altura = float(input("Digite a sua altura: "))
+
+print(f"nome: {nome} idade: {idade} altura: {altura}")
