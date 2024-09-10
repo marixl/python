87828b9d9ecd6724a38ae53d8186553141c50290
marixl <marianavalenca72@gmail.com>
2024-09-10 10:42:19 -0300

    Add files via upload

---
+++ 3-aula3.py
@@ -0,0 +1,16 @@
+# Variáveis em Pyton 
+# Não há necessidade de declará-las no início do programa 
+# basta criar no meio do algoritmo com a atribuição 
+
+teste = "Mariana"
+print (teste) #Imprime a string "Nicolas"
+print (type(teste)) #Imprime o tio de váriavel, string 
+teste = 10
+print (teste) # Imprime o número 10
+print (type(teste)) #Imprime o tipo de variável, que é int 
+teste - 1.74
+print (teste) #Imprime o número 1.74
+print (type(teste)) #Imprime o itpo de variável, que é float 
+teste = True 
+print (teste) #Imprimir o valor True
+print(type (teste)) #imprime o tipo de variável, que é bool 
---
+++ 5-aula5.py
@@ -0,0 +1,13 @@
+# Tipos de Dados em Pyton 
+#Inteiros 
+inteiro = 10
+print (inteiro)
+#Floats
+float = 10.5
+print(float)
+# Strings 
+strings = "Olá mundo!"
+print(string)
+# Booleanos 
+booleano = True 
+print(booleano)
