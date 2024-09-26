d3396859314e6d44961dacfc0b041a3a5d6e8fb7
Marianah <marianavalenca72@gmail.com>
2024-09-26 11:29:47 -0300

    Add files via upload

---
+++ par-ou-impar.py
@@ -0,0 +1,7 @@
+# Crie um algoritimo que receba um inteiro e
+# identidade se ele é par ou impar.
+numero = int(input("Digite um número: "))
+if (numero % 2 == 0):
+    print(f"O número {numero} é par.")
+else:
+    print(f"O número {numero} é impar.")
